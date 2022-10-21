635399be569c696e70e16382eb45182f6470b8f4
Rati1324 <nonima444@protonmail.com>
2022-10-21 13:29:32 +0400

    _

---
+++ homework_2/homework_2/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using homework_2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework_2.Controllers {
+	public class HomeController : Controller {
+		public List<User> GetUsers() {
+			List<User> users = new List<User>() {
+				new User { Id = 1, Name = "John", Age = 20 },
+				new User { Id = 2, Name = "John 2", Age = 22 },
+				new User { Name = "John 2", Age = 22 },
+			};
+			return users;
+		}
+
+		public ActionResult Index() {
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Index(User user) {
+			if (ModelState.IsValid) {
+				return RedirectToAction("Message");
+			}
+			return View();
+		}
+
+		public ActionResult Message() {
+			return View();
+		}
+	}
+}
---
+++ homework_2/homework_2/Models/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+
+namespace homework_2.Models {
+	public class User {
+		public int Id { get; set; }
+
+		[Display(Name = "User Name")]
+		[Required(ErrorMessage = "This field is required")]
+		[StringLength(5)]
+		public string Name { get; set; }
+
+		[Required(ErrorMessage = "This field is required")]
+		[Range(18, 100, ErrorMessage = "Must be between 18 and 100")]
+		public int Age { get; set; }
+
+		[Required(ErrorMessage = "This field is required")]
+		[RegularExpression(".+@.+\\..+", ErrorMessage = "Wrong Email format")]
+		public string Email { get; set; }
+
+		[Required]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+		[Required]
+		[Display(Name="Confirm password")]
+		[Compare("Password", ErrorMessage = "Passwords don't match")]
+		[DataType(DataType.Password)]
+		public string ConfirmPassword { get; set; }
+	}
+}
