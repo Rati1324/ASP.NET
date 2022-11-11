2cbf3a3ea7cbab27ff39e95c2aabc78ba027a581
Rati1324 <rati.mtsituri@gau.edu.ge>
2022-11-11 17:17:31 +0400

    _

--- Midterm/Midterm/Controllers/CarController.cs
+++ Midterm/Midterm/Controllers/CarController.cs
@@ -3,6 +3,7 @@ using Midterm.AuthData;
 using Midterm.DB;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -24,12 +25,39 @@ namespace Midterm.Controllers
 		}
 
 		[HttpPost]
-		public ActionResult InsertCar(Car car) {
+		public ActionResult InsertCar(Car car=null) {
 			if (ModelState.IsValid) {
 				DB.Cars.Add(car);
 				DB.SaveChanges();
+				return RedirectToAction("GetCars");
 			}
 			return View();
+		}
+
+		public ActionResult EditCar(int carId) {
+			ViewBag.Car = DB.Cars.Where(i => i.id == carId).First();
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult EditCar(Car car) {
+			if (ModelState.IsValid) {
+				Car dbCar = DB.Cars.Where(i => i.id == car.id).First();
+				dbCar.name = car.name;
+				dbCar.speed = car.speed;
+				dbCar.horsePower = car.horsePower;
+				dbCar.price = car.price;
+				DB.SaveChanges();
+				return RedirectToAction("GetCars");
+			}
+			return View();
+		}
+
+		public ActionResult DeleteCar(int carId) {
+			Car car = DB.Cars.Where(i => i.id == carId).First();
+			DB.Cars.Remove(car);
+			DB.SaveChanges();
+			return RedirectToAction("GetCars");
 		}
     }
 }
