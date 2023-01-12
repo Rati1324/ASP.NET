cbe8f3c0960fc83498d25cbd2be5d7e8d4ad7442
rati1324 <rati.mtsituri@gau.edu.ge>
2023-01-12 20:07:45 +0400

    _

--- WebApplication4/WebApplication4/Controllers/ProductController.cs
+++ WebApplication4/WebApplication4/Controllers/ProductController.cs
@@ -56,5 +56,10 @@ namespace WebApplication4.Controllers
                 return View();
             }
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
